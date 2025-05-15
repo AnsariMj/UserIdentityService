b3e0778f56f2eb17b68af4d0c7c2f97974ad315f
mjansari <mjansari@devfinity.com>
2025-05-15 15:55:33 +0545

    Testing framework added xUint

---
+++ UserIdentityService.Test/Blogs/CreateBlogCommandTest.cs
@@ -0,0 +1,20 @@
+﻿namespace UserIdentityService.Test.Blogs;
+
+public class CreateBlogCommandTest
+{
+    public void CreateBlogCommand_validRequest_ShouldCreateBlg(string Title,
+                                                                string Content,
+                                                                string Iamge,
+                                                                string AuthorName,
+                                                                string CreatedAt,
+                                                                string UpdatedAt,
+                                                                string PublishedAt,
+                                                                string IsPublished)
+    {
+        //Arrange
+
+
+        //Act
+        //Assert
+    }
+}
